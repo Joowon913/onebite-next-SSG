9ce38bba81e8594376013fb26de5b81e06f3abb7
Joowon <kimjw0913@gmail.com>
2024-12-13 22:09:41 +0900

    SSG 정적 경로 적용하기

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,52 @@
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode, useEffect, useState } from "react";
+import BookItem from "@/components/book-item";
+import { useRouter } from "next/router";
+import { BookData } from "@/types";
+import fetchBooks from "@/lib/fetch-books";
+// import { GetStaticPropsContext } from "next";
+
+// export const getStaticPrpos = async (
+//   context: GetStaticPropsContext
+// ) => {
+//   const q = context.query.q;
+//   const books = await fetchBooks(q as string);
+
+// 	return {
+// 		props: {
+//       books,
+//     },
+// 	};
+// };
+
+export default function Page() {
+	const [books, setBooks] = useState<BookData[]>([]);
+
+	const router = useRouter();
+	const q = router.query.q;
+
+	const fetchSearchResult = async() => {
+		const data = await fetchBooks(q as string);
+		setBooks(data);
+	}
+
+	useEffect(() => {
+		if (q) {
+			// 검색 결과를 불러오는 로직
+			fetchSearchResult();
+		}
+
+	}, [q]);
+
+	return (
+		<div>
+			{books.map((book) => (
+				<BookItem key={book.id} {...book} />
+			))}
+		</div>
+	);
+}
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>
+}
